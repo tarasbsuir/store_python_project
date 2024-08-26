2f5a636c274916af137ad0875a37786a3b59a14d
Taras Yatsenko <tarasbsuir@gmail.com>
2024-08-26 22:16:34 +0300

    main + is

--- internet_store.py
+++ internet_store.py
@@ -2,3 +2,5 @@ from sqlite3 import Cursor
 
 
 # Class describes internet store
+
+pass
--- main.py
+++ main.py
@@ -2,3 +2,12 @@ import internet_store
 
 
 ''' Main class for internet store. '''
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    print("main.py - run as a standalone program.\n")
+    main()
+else:
+    print("main.py - run as a built-in module.")
