6b8ea5d5598da0a9180f843539005b7d3529e44a
Taras Yatsenko <44841362+tarasbsuir@users.noreply.github.com>
2024-08-28 17:04:15 +0300

    Internet_store.py updated

--- internet_store.py
+++ internet_store.py
@@ -4,6 +4,12 @@
 import sqlite3 as s
 import getpass
 
+EXIT_MSG = ('''
+             |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+             |||                        SEE YA!                      |||
+             |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+            ''')
+
 
 class Internet_Store:
     '''Class description'''
@@ -68,34 +74,56 @@ class App:
             print(
                 '''MENU:
 1 - Sign in
+2 - Register (пока не работает)
 0 - Exit'''
             )
-            user_operation = input("Input operation: ") # Надо добавить вход по логину и паролю в БД
+            user_operation = input("Input operation: ")
             
             if user_operation == "1":
                 user_name = input("Enter user name: ")
                 user_pass = getpass.getpass("Enter user password: ")
+                
+                
                 conn = s.connect('internet_store.db') # Может создать отдельный класс DB_Connect?
                 c = conn.cursor()
                 c.execute('SELECT user_role from users where user_name = ? and user_pass = ?',
                           (user_name, user_pass))
                 result_of_select = c.fetchall()
                 
+                
                 if result_of_select == []:
                     print('USER NOT FOUND or password is incorrect.')
                 
-                elif result_of_select[0] == (0,):
+                # ADMIN MENU.
+                elif result_of_select[0] == (0,):  # обернуть в цикл while
                     print(
                 '''ADMIN MENU: 
 1 - add user
 2 - del user
 3 - block user
 4 - upd user pass
+5 - show all users
 0 - Exit'''
                     )
                     user_operation = input("Input operation: ")
-                    
-                elif result_of_select[0] == (1,):
+                    if user_operation == '1':
+                        print("ADD USER")
+                    elif user_operation == '2':
+                        print('del user')
+                    elif user_operation == '3':
+                        print("block user")
+                    elif user_operation == '4':
+                        print('upd user pass')
+                    elif user_operation == '5':
+                        print('all users')
+                    elif user_operation == "0":
+                        self.cond = False
+                        print(EXIT_MSG)
+                    else:
+                        print("Wrong command!!!")
+                
+                # MANAGER MENU.  
+                elif result_of_select[0] == (1,):  # обернуть в цикл while
                     print(
                 '''MANAGER MENU: 
 1 - add product
@@ -110,8 +138,33 @@ class App:
 0 - Exit'''
                     )
                     user_operation = input("Input operation: ")
-                    
-                elif result_of_select[0] == (2,):
+                    if user_operation == '1':
+                        print("ADD product")
+                        Internet_Store().add_product()
+                    elif user_operation == '2':
+                        print('del product')
+                    elif user_operation == '3':
+                        print("show all products")
+                    elif user_operation == '4':
+                        print('find product by name')
+                    elif user_operation == '5':
+                        print('find product by article (id)')
+                    elif user_operation == '6':
+                        print('change product price')
+                    elif user_operation == '7':
+                        print('change product status')
+                    elif user_operation == '8':
+                        print('change product name')
+                    elif user_operation == '9':
+                        print('change product description')
+                    elif user_operation == "0":
+                        self.cond = False
+                        print(EXIT_MSG)
+                    else:
+                        print("Wrong command!!!")
+                
+                # WORK USER MENU. 
+                elif result_of_select[0] == (2,):  # обернуть в цикл while
                     print(
                 '''USER MENU: 
 1 - show all products
@@ -121,28 +174,30 @@ class App:
 0 - Exit'''
                     )
                     user_operation = input("Input operation: ")
-                
-                
-                # # Пригодится else?    
-                # else:
-                #      print('USER NOT FOUND')
-                #      print(result_of_select)
-                # try:
-                #     pass
-                #     # Тут подключение к БД и проверка логина и пароля
-                # except Exception as e:  # Обработать ошибки с подключением к БД?
-                #     print(e)
-                # else:
-                #     pass  # Что-то сделать надо!
-                
+                    if user_operation == '1':
+                        print("show all products")
+                    elif user_operation == '2':
+                        print('find product by name')
+                    elif user_operation == '3':
+                        print("find product by article")
+                    elif user_operation == '4':
+                        print('buy product')
+                    elif user_operation == "0":
+                        self.cond = False
+                        conn.close()
+                        print(EXIT_MSG)
+                        
+                # USER BLOCKED.
+                elif result_of_select[0] == (3,):
+                    print("User blocked")
+                    
+            # REG NEW USER.        
+            elif user_operation == "2":
+                print("REG NEW USER")
 
             elif user_operation == "0":
                 self.cond = False
                 conn.close()
-                print('''
-             |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-             |||                        SEE YA!                      |||
-             |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-             ''')
+                print(EXIT_MSG)
             else:
                 print("Wrong command!!!")
